22c61cb611b8a35c93bbdcf7825e36f5b5b530c7
DamonHayes <92762805+DamonHayes@users.noreply.github.com>
2022-02-23 20:52:27 -0600

    sending index and show data

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to an Awesome App about Breads!</h1>')
 })
 
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 app.get('*', (req, res) => {
   res.send('<h1>404 NOT FOUND</h1>')
 })
-
-// Breads
-const breadsController = require('./controllers/breads_controllers.js')
-app.use('/breads', breadsController)
 
 // LISTEN
 app.listen(PORT, () => {
